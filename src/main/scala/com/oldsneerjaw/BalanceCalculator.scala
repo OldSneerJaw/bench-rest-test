7df9102ff60bc923292f3ccf8b7ead7d01f33a9b
Joel Andrews <oldsneerjaw@gmail.com>
2017-05-14 11:33:20 -0700

    Switch to a linked hash map for storing running totals of daily balances
    Using a LinkedHashMap rather than a TreeMap means that the act of calculating all daily balances is now an O(n) operation rather than O(n * log n). While the preceding list sort already made this method O(n * log n) in either case, it is a minor change to improve the efficiency of the actual calculation operation.
    
    The more significant change may be how the missing days are filled in; previously it relied upon the `lastOption` method to determine what was the last day, but this is actually an O(log n) operation in the case of a TreeMap and, even worse, an O(n) operation in the case of a LinkedHashMap. Instead of incurring the cost of calling `lastOption` on every single iteration (potentially making the efficiency of calculating all balances O(n * log n * log n) or O(n * n)), the previous day's entry is simply carried forward to each iteration of the fold operation, making its efficiency bound to the number of missing days rather than the number of missing days plus the number of preceding balance entries.

--- src/main/scala/com/oldsneerjaw/BalanceCalculator.scala
+++ src/main/scala/com/oldsneerjaw/BalanceCalculator.scala
@@ -3,7 +3,7 @@ package com.oldsneerjaw
 import org.joda.time.{DateTime, Duration}
 import play._
 
-import scala.collection.immutable.TreeMap
+import scala.collection.mutable
 
 /**
   * Calculates the total account balance of a collection of transaction summaries.
@@ -11,6 +11,8 @@ import scala.collection.immutable.TreeMap
 class BalanceCalculator {
 
   private val logger = Logger.of(getClass)
+
+  private type DailyBalanceEntry = (Long, BigDecimal)
 
   /**
     * Determines the total balance taking into account all transactions.
@@ -45,40 +47,44 @@ class BalanceCalculator {
     // Sort all transactions by date
     val sortedTransactions = allTransactions.sortBy(_.date.getMillis)
 
-    val initialTotals = (TreeMap.empty[Long, BigDecimal], BigDecimal(0))
-    val (computedDailyBalanceMap, _) = sortedTransactions.foldLeft(initialTotals) { (runningTotals, transaction) =>
-      val (dailyBalanceMap, totalBalance) = runningTotals
+    // Uses a linked hash map to store the daily balances for two reasons: (1) to make retrieving and updating the existing balance for a
+    // given day a constant time operation and (2) so that chronological order of transactions is preserved from the sort performed earlier
+    val dailyBalanceMap = new mutable.LinkedHashMap[Long, BigDecimal]()
 
-      val updatedBalance = totalBalance + parseAmount(transaction.amount)
-      val filledInDailyBalanceMap = fillInMissingDays(dailyBalanceMap, transaction.date)
-      val updatedDailyBalanceMap = filledInDailyBalanceMap.+((transaction.date.getMillis, updatedBalance))
+    sortedTransactions.foldLeft[Option[DailyBalanceEntry]](None) { (previousEntryOption, transaction) =>
+      val runningBalance = previousEntryOption.map(_._2).getOrElse(BigDecimal(0))
 
-      (updatedDailyBalanceMap, updatedBalance)
+      val updatedBalance = runningBalance + parseAmount(transaction.amount)
+      val currentEntry = new DailyBalanceEntry(transaction.date.getMillis, updatedBalance)
+
+      dailyBalanceMap ++= getMissingDays(transaction.date, previousEntryOption) += currentEntry
+
+      Option(currentEntry)
     }
 
     // Convert the map to a sequence
-    computedDailyBalanceMap.toSeq map { dailyBalanceEntry =>
+    dailyBalanceMap.toSeq map { dailyBalanceEntry =>
       DailyBalance(new DateTime(dailyBalanceEntry._1), dailyBalanceEntry._2)
     }
   }
 
-  private def fillInMissingDays(dailyBalanceMap: TreeMap[Long, BigDecimal], currentDate: DateTime): TreeMap[Long, BigDecimal] = {
-    dailyBalanceMap.lastOption match {
+  private def getMissingDays(currentDate: DateTime, previousEntryOption: Option[DailyBalanceEntry]): Seq[DailyBalanceEntry] = {
+    previousEntryOption match {
       case None =>
         // The map is empty, so add the first day, which should be one day before the current date
-        dailyBalanceMap.+((currentDate.minusDays(1).getMillis, BigDecimal(0)))
+        Seq(currentDate.minusDays(1).getMillis -> BigDecimal(0))
       case Some(lastEntry) =>
         val lastDate = new DateTime(lastEntry._1)
         if (currentDate.minusDays(1).isAfter(lastDate)) {
           // One or more intervening days had no transactions - fill them in with the same balance as the previous transaction date
           val duration = new Duration(lastDate, currentDate)
           val durationRange = 1 to duration.getStandardDays.toInt
-          durationRange.foldLeft(dailyBalanceMap) { (runningDailyBalanceMap, days) =>
-            runningDailyBalanceMap.+((lastDate.plusDays(days).getMillis, lastEntry._2))
+          durationRange map { days =>
+            new DailyBalanceEntry(lastDate.plusDays(days).getMillis, lastEntry._2)
           }
         } else {
           // There are no missing days to fill in
-          dailyBalanceMap
+          Seq.empty
         }
     }
   }
@@ -94,3 +100,4 @@ class BalanceCalculator {
     }
   }
 }
+
